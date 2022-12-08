2fefa033643d000b88b39b5695ca2d10530a7edc
1478952 <1478952@bubblecon.io>
2022-12-08 10:13:52 +0900

    BBC-680 tp08b 구현

--- src/types/pageTemplate.ts
+++ src/types/pageTemplate.ts
@@ -344,6 +344,18 @@ export type TP08A = {
 
 type TP08AContent = ImagesContent | HtmlContent | SentenceWordContent;
 
+export type TP08B = {
+  id: ID;
+  title: string;
+  description: string;
+  template: {
+    type: "TP08B";
+    contents: TP08BContent[];
+  };
+};
+
+type TP08BContent = ImagesContent | HtmlContent | AudioRecordContent;
+
 export type TP08G = {
   id: ID;
   title: string;
