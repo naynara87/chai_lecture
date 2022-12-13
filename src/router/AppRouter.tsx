8a99899a647e716d3cebd6855beb7a526941874b
key <key9559@naver.com>
2022-12-13 17:50:24 +0900

    BBC-708 단어테스트 결과 화면

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -5,6 +5,7 @@ import CornerPage from "../components/pages/CornerPage";
 import MetaTestChoiceWord from "../components/pages/MetaTestChoiceWord";
 import MetaTestWordTest from "../components/pages/MetaTestWordTest";
 import MetaTestWordTestGrade from "../components/pages/MetaTestWordTestGrade";
+import MetaTestWordTestReport from "../components/pages/MetaTestWordTestReport";
 import { CORNER_LIST_URL } from "../constants/url";
 
 const AppRouter = () => {
@@ -17,9 +18,10 @@ const AppRouter = () => {
           element={<CornerPage />}
         />
         <Route path="*" element={<Navigate to={CORNER_LIST_URL} replace />} />
-        <Route path={'test-main'} element={<MetaTestChoiceWord />} />
+        <Route path={'test-word-choice'} element={<MetaTestChoiceWord />} />
         <Route path={'test-word-test'} element={<MetaTestWordTest />} />
         <Route path={'test-word-grade'} element={<MetaTestWordTestGrade />} />
+        <Route path={'test-word-report'} element={<MetaTestWordTestReport />} />
       </Routes>
     </HashRouter>
   );
