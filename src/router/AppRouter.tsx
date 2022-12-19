0b183e030630167d112eb4392477a91ff7a73461
key <key9559@naver.com>
2022-12-19 18:00:39 +0900

    BBC-766 메타인지 문장형 작업중

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -7,6 +7,7 @@ import MetaTestWordTest from "../components/pages/MetaTestWordTest";
 import MetaTestWordTestGrade from "../components/pages/MetaTestWordTestGrade";
 import MetaTestWordTestReport from "../components/pages/MetaTestWordTestReport";
 import MetaTestWordLearning from "../components/pages/MetaTestWordLearning";
+import MetaTestSentence from "../components/pages/MetaTestSentence";
 import { CORNER_LIST_URL } from "../constants/url";
 
 const AppRouter = () => {
@@ -19,11 +20,18 @@ const AppRouter = () => {
           element={<CornerPage />}
         />
         <Route path="*" element={<Navigate to={CORNER_LIST_URL} replace />} />
+        {/* 단어 테스트 */}
         <Route path={"test-word-choice"} element={<MetaTestWordChoice />} />
+        {/* 단어 테스트01 ~ 단어 테스트05, 확인 문제01 */}
         <Route path={"test-word-test"} element={<MetaTestWordTest />} />
+        {/* 단어 테스트06, 확인 문제02 */}
         <Route path={"test-word-grade"} element={<MetaTestWordTestGrade />} />
+        {/* 단어 테스트 결과-아는 단어, 단어 테스트 결과-모르는 단어 */}
         <Route path={"test-word-report"} element={<MetaTestWordTestReport />} />
+        {/* 단어 학습-학습단어01, 02, 단어테스트 최종 결과 */}
         <Route path={"test-word-learning"} element={<MetaTestWordLearning />} />
+        {/* TP03H */}
+        <Route path={"test-sentence"} element={<MetaTestSentence />} />
       </Routes>
     </HashRouter>
   );
