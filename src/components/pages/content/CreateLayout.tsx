666db81b98ea367f2764f5b5eec3c58e0508ac14
TaehwanGo <gth1123@bubblecon.io>
2022-12-29 14:02:31 +0900

    BBC-798 저작도구 라우팅 및 레이아웃

---
+++ src/components/pages/content/CreateLayout.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "@emotion/styled";
+import useCreateContent from "../../../hooks/useCreateContent";
+import { TemplateType } from "../../../types/appData";
+import { layouts } from "../../../data/contentCreate";
+import { useNavigate } from "react-router-dom";
+import { CREATE_CONTENT_COMPONENTS_URL } from "../../../constants/url";
+
+const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px 0;
+`;
+
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+`;
+
+const LayoutContainer = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 700px;
+`;
+
+const LayoutItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+  cursor: pointer;
+`;
+
+const CreateLayout = () => {
+  const { setContentLayout } = useCreateContent();
+
+  const navigate = useNavigate();
+
+  const handleLayoutClick = (layoutName: TemplateType) => {
+    setContentLayout(layoutName);
+    console.log("selected layout: ", layoutName);
+    navigate(CREATE_CONTENT_COMPONENTS_URL);
+  };
+
+  return (
+    <div>
+      <Header>
+        <h1>레이아웃 설정</h1>
+        <h4>페이지 레이아웃을 선택해주세요</h4>
+      </Header>
+      <Main>
+        <LayoutContainer>
+          {layouts.map((layout) => (
+            <LayoutItem key={layout.name} onClick={() => handleLayoutClick(layout.name)}>
+              <h3>{layout.name}</h3>
+              <img src={layout.image} alt={layout.name} draggable={false} />
+            </LayoutItem>
+          ))}
+        </LayoutContainer>
+      </Main>
+    </div>
+  );
+};
+
+export default CreateLayout;
---
+++ src/data/contentCreate/index.ts
@@ -0,0 +1,24 @@
+import { ContentLayoutType } from "../../types/createContent";
+
+export const layouts: ContentLayoutType[] = [
+  {
+    name: "TP01A",
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    name: "TP02",
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    name: "TP03",
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    name: "TP04",
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    name: "TP05",
+    image: "https://via.placeholder.com/150",
+  },
+];
