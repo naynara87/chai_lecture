f364d88f2def39574d1f152cc5ef666d3d5a50f1
TaehwanGo <gth1123@bubblecon.io>
2023-01-30 11:09:32 +0900

    BBC-867 util import 에러 수정

--- packages/chai-ui/src/utils/url.ts
+++ packages/chai-ui/src/utils/url.ts
@@ -1,6 +1,11 @@
-import { ID } from "chai-ui";
+import { ID } from "../types";
 
 // page
-export const getPageUrl = (courseId: ID, lessonId: ID, cornerId: ID, pageId: ID) => {
+export const getPageUrl = (
+  courseId: ID,
+  lessonId: ID,
+  cornerId: ID,
+  pageId: ID
+) => {
   return `/course/${courseId}/lesson/${lessonId}/corner/${cornerId}/page/${pageId}`;
 };
