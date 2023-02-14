04ae972f0945a9b66f9384feccd83d35bfea6941
key <key9559@naver.com>
2023-02-14 13:43:08 +0900

    BBC-925 네비게이션

--- packages/chai-contents-view/src/components/molecules/LayoutFooter.tsx
+++ packages/chai-contents-view/src/components/molecules/LayoutFooter.tsx
@@ -7,46 +7,43 @@ import imgHdCharacter from "../../images/img/cha_wini_wink.png";
 
 const LayoutFooter = () => {
   return (
-    <footer className="cai-ft">
-      {/* position: absolute */}
-      <div className="lnb-wrap">
-        {/* TODO: 클릭시 cai-nav-container가 active */}
-        <button className="btn-text">
+    <div>
+      <footer className="cai-ft">
+        {/* position: absolute */}
+        {/* TODO: 클릭시 스스로와 cai-nav-container에 active 추가 */}
+        <button className="btn-ft-text active">
           학습순서
           <img src={iconOpen} alt="" className="icon icon-open" />
           <img src={iconClose} alt="" className="icon icon-close" />
         </button>
-        <div className="cai-nav-container active">
-          <img src={imgHdCharacter} alt="위니" />
-          <nav className="cai-lnb-wrap">
-            <ul className="cai-lnb-list-wrap">
-              <li className="cai-lnb-list active">
-                복습
-                <div className="cai-lnb-sub-wrap">
-                  <a href="" className="cai-lnb-sub-link active">회화</a>
-                  <a href="" className="cai-lnb-sub-link">문법퀴즈</a>
-                  <a href="" className="cai-lnb-sub-link">지난레슨단어</a>
-                </div>
-              </li>
-              <li className="cai-lnb-list">
-                코너명
-                <div className="cai-lnb-sub-wrap">
-                  <a href="" className="cai-lnb-sub-link">헤더</a>
-                  <a href="" className="cai-lnb-sub-link">헤더</a>
-                  <a href="" className="cai-lnb-sub-link">헤더</a>
-                </div>
-              </li>
-            </ul>
-            <button className="txt-btn">학습 중단</button>
-          </nav>
+        <div className="ft-conts-wrap">
+          <button className="ft-icon-btn" disabled><img src={iconLeft} alt="이전" /></button>
+          <span className="txt"><b>{'1'}</b><small> / {'10'}</small></span>
+          <button className="ft-icon-btn"><img src={iconRight} alt="다음" /></button>
         </div>
+      </footer>
+
+      <div className="cai-nav-container active">
+        <img src={imgHdCharacter} alt="위니" className="character" />
+        <nav className="cai-nav-wrap">
+          <ul className="cai-nav-list-wrap">
+            {/* TODO: 반복영역. 열려있을 때 active 추가됨 */}
+            <li className="cai-nav-list active">
+              <div className="cai-nav-ttl">{'복습'}</div>
+              <div className="cai-nav-sub-wrap">
+                {/* TODO: 현재 페이지에 active */}
+                <a href="" className="cai-nav-sub-link active">{'회화'}</a>
+                <a href="" className="cai-nav-sub-link">{'문법퀴즈'}</a>
+                <a href="" className="cai-nav-sub-link">{'지난레슨단어'}</a>
+              </div>
+            </li>
+          </ul>
+          <div className="btn-wrap">
+            <button className="btn-fill-black">학습 중단</button>
+          </div>
+        </nav>
       </div>
-      <div className="ft-conts-wrap">
-        <button className="ft-icon-btn" disabled><img src={iconLeft} alt="이전" /></button>
-        <span className="txt"><b>{ '1' }</b><small> / { '10' }</small></span>
-        <button className="ft-icon-btn"><img src={iconRight} alt="다음" /></button>
-      </div>
-    </footer>
+    </div>
   );
 };
 
