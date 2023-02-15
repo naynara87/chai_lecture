95821def3a8947a5aa9d15bf4bf74c8bc8590281
key <key9559@naver.com>
2023-02-15 12:29:26 +0900

    BBC-936 헤더 변수들 이름 통일 헤더 꿍이

--- packages/chai-contents-view/src/components/molecules/LayoutHeader.tsx
+++ packages/chai-contents-view/src/components/molecules/LayoutHeader.tsx
@@ -1,46 +1,47 @@
 import styled from "@emotion/styled";
 import React from "react";
-import iconLevel1 from "../../images/img/hd_red.svg";
-import iconLevel2 from "../../images/img/hd_orange.svg";
-import iconLevel3 from "../../images/img/hd_yellow.svg";
-import iconLevel4 from "../../images/img/hd_green.svg";
-import iconLevel5 from "../../images/img/hd_blue.svg";
-import iconLevel6 from "../../images/img/hd_navy.svg";
-import iconLevel7 from "../../images/img/hd_purple.svg";
+import ImgCharacter from "../../images/img/cha_kkungi_positive.png";
+import IconLevel1 from "../../images/img/hd_red.svg";
+import IconLevel2 from "../../images/img/hd_orange.svg";
+import IconLevel3 from "../../images/img/hd_yellow.svg";
+import IconLevel4 from "../../images/img/hd_green.svg";
+import IconLevel5 from "../../images/img/hd_blue.svg";
+import IconLevel6 from "../../images/img/hd_navy.svg";
+import IconLevel7 from "../../images/img/hd_purple.svg";
 
 // TODO: 레벨별 컬러는 헤더에서만 사용
-const Level1Main = '#EE4141';
-const Level1Sub = '#F89E9E';
-const Level2Main = '#FF6700';
-const Level2Sub = '#FFA861';
-const Level3Main = '#FFB900';
-const Level3Sub = '#FFD86C';
-const Level4Main = '#1FB65D';
-const Level4Sub = '#81D19D';
-const Level5Main = '#3D89FA';
-const Level5Sub = '#8CB5EF';
-const Level6Main = '#3C53A7';
-const Level6Sub = '#778AD0';
-const Level7Main = '#9A45EF';
-const Level7Sub = '#B991FA';
+const MainLevel1 = '#EE4141';
+const SubLevel1 = '#F89E9E';
+const MainLevel2 = '#FF6700';
+const SubLevel2 = '#FFA861';
+const MainLevel3 = '#FFB900';
+const SubLevel3 = '#FFD86C';
+const MainLevel4 = '#1FB65D';
+const SubLevel4 = '#81D19D';
+const MainLevel5 = '#3D89FA';
+const SubLevel5 = '#8CB5EF';
+const MainLevel6 = '#3C53A7';
+const SubLevel6 = '#778AD0';
+const MainLevel7 = '#9A45EF';
+const SubLevel7 = '#B991FA';
 
 const HdContWrap = styled.div`
 /* TODO: width: 100%일 때 after의 배경색을 div의 배경색으로 적용 */
   width: 42%;
-  background-image: url("${iconLevel1}");
+  background-image: url("${IconLevel1}");
 
   &:after {
-		background-color: ${Level1Main};
+		background-color: ${MainLevel1};
   }
 `;
 
 const CaiHeader = styled.header`
-  background-color: ${Level1Sub};
+  background-color: ${SubLevel1};
 `;
 
 const HdChaWrap = styled.div`
   .txt {
-    color: ${Level1Main};
+    color: ${MainLevel1};
   }
 `;
 
@@ -53,11 +54,11 @@ const LayoutHeader = () => {
         {/* 우선 클래스 none 로 가림 */}
         <HdChaWrap className="hd-cha-wrap second-half none">
           <p className="txt">{'학습목표 자가체크'}</p>
-          <img src="/images/img/cha_kkungi_positive.png" alt="꿍이" className="img" />
+          <img src={ImgCharacter} alt="꿍이" className="img" />
         </HdChaWrap>
         <span className="corner-name">{'학습 시작'}</span>
         <HdChaWrap className="hd-cha-wrap first-half">
-          <img src="/images/img/cha_kkungi_positive.png" alt="꿍이" className="img" />
+          <img src={ImgCharacter} alt="꿍이" className="img" />
           <p className="txt">{'학습목표 자가체크'}</p>
         </HdChaWrap>
       </HdContWrap>
