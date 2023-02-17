8825f8fc7970a3127cdb1afec20c2ff2d4ca8c89
key <key9559@naver.com>
2023-02-17 18:06:07 +0900

    pub/BBC-942 회화부분 컴포넌트 완료

--- packages/chai-contents-view/src/components/molecules/ComponentConversation.tsx
+++ packages/chai-contents-view/src/components/molecules/ComponentConversation.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ImgCharacter from "../../images/img/cha_didi_glasses.png";
 import IconSpeaker from "../../images/icon/icon_speaker_white.svg";
+import ImgProfileDefault from "../../images/img/img_profile_default.png";
+import ImgTemp01 from "../../images/img/temp_profile01.png";
 import ComponentToggle from "./ComponentToggle";
 
 const ComponentConversation = () => {
@@ -8,7 +10,8 @@ const ComponentConversation = () => {
   return (
     <div className="layout-panel-wrap grid37">
       <div className="layout-panel side-panel">
-        {/* ComponentComtsinfo */}
+        {/* 1 ComponentComtsinfo */}
+        {/* FIXME: 컴포넌트 수정예정 : 음원재생버튼 모양 변경(원이 아닌 모양으로), 이미지 변경 */}
         <div className="cont-info-wrap">
           <div className="btns-wrap">
             <button className="btn-icon-with-text">
@@ -23,28 +26,64 @@ const ComponentConversation = () => {
             <img src={ImgCharacter} alt="" className="img" />
           </div>
         </div>
-        {/* end ComponentComtsinfo */}
+        {/* end 1 ComponentComtsinfo */}
       </div>
-      <div className="layout-panel wide-panel">
-        {/* ComponentTitle */}
+      {/* 230217 회화는 단일 컴포넌트여서 스타일을 위해 conversation-panel-wrap 클래스 추가함 */}
+      <div className="layout-panel wide-panel conversation-panel-wrap">
+        {/* 230216 회화의 제목이 있을 때에만 사용 */}
+        {/* 2 ComponentTitle */}
+        {/* FIXME: 컴포넌트 수정예정 : 앞 아이콘 -> 기호 */}
         <h2 className="conts-ttl">{'대화 내용을 잘 들어보세요'}</h2>
-        {/* end ComponentTitle */}
+        {/* end 2 ComponentTitle */}
+        {/* 230216 회화에서 한어병음과 뜻의 보임 여부를 선택할 때 사용 */}
+        {/* 3 ComponentToggle */}
         <div className="toggles-wrapper">
-          {/* ComponentToggle */}
           <div className="inp-toggle-wrap">
             <span className="toggle-name">한어병음</span>
             <input type="checkbox" name="input1" id="input1" className="toggle-input none" />
-            <label className="toggle-label">{'보이기'}</label>
+            <label htmlFor="input1" className="toggle-label">보이기</label>
           </div>
-          {/* end ComponentToggle */}
-          {/* ComponentToggle */}
           <div className="inp-toggle-wrap">
             <span className="toggle-name">뜻</span>
-            <input type="checkbox" name="input1" id="input1" className="toggle-input none" />
-            <label className="toggle-label">{'보이기'}</label>
+            <input type="checkbox" name="input2" id="input2" className="toggle-input none" />
+            <label htmlFor="input2" className="toggle-label">보이기</label>
           </div>
-          {/* end ComponentToggle */}
         </div>
+        {/* end ComponentToggle */}
+        {/* 230217 회화영역 */}
+        <ul className="conversation-wrapper">
+          {/* speech bubble */}
+          {/* TODO: 음성이 재생될 때 active 가 추가됨(화자표시 애니메이션) */}
+          <li className="conversation-wrap active">
+            <div className="img-wrap">
+              {/* TODO: 누르면 단일 음성이 재생됨 */}
+              <div className="img-round">
+              <button className="btn-profile"><img src={ImgProfileDefault} alt="" className="profile" /></button>
+              </div>
+            </div>
+            <div className="txt-wrap">
+              <div className="name">{ '왕리리' }</div>
+              <div className="chinese">{ '今天刮风，下雪，很冷。' }</div>
+              <div className="pinyin">{ 'Jīntiān guā fēng, xià xuě, hěn lěng.' }</div>
+              <div className="mean">{ '오늘은 바람이 불고, 눈이 내려서 추워.' }</div>
+            </div>
+          </li>
+          {/* end speech bubble */}
+          <li className="conversation-wrap">
+            <div className="img-wrap">
+              {/* TODO: 누르면 단일 음성이 재생됨 */}
+              <div className="img-round">
+                <button className="btn-profile"><img src={ImgTemp01} alt="" className="profile" /></button>
+              </div>
+            </div>
+            <div className="txt-wrap">
+              <div className="name">{ '김민호' }</div>
+              <div className="chinese">{ '我觉得这里的冬天没有中国那么冷。' }</div>
+              <div className="pinyin">{ 'Wǒ juédé zhèlǐ de dōngtiān méiyǒu zhòng guó nàme lěng.' }</div>
+              <div className="mean">{ '나는 여기 겨울이 중국만큼 춥지 않은 것 같아.' }</div>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- packages/chai-contents-view/src/components/molecules/ComponentToggle.tsx
+++ packages/chai-contents-view/src/components/molecules/ComponentToggle.tsx
@@ -3,10 +3,13 @@ import React from "react";
 const ComponentToggle = () => {
 
   return (
-    <div className="inp-toggle-wrap">
-      <span className="toggle-name">한어병음</span>
-      <input type="checkbox" name="input1" id="input1" className="toggle-input none" />
-      <label className="toggle-label">{ '보이기' }</label>
+    <div className="toggles-wrapper">
+      {/* TODO: 여러개가 들어가면 inp-toggle-wrap 이 반복되서 들어감 */}
+      <div className="inp-toggle-wrap">
+        <span className="toggle-name">{ '한어병음' }</span>
+        <input type="checkbox" name="input1" id="input1" className="toggle-input none" />
+        <label htmlFor="input1" className="toggle-label">보이기</label>
+      </div>
     </div>
   );
 };
