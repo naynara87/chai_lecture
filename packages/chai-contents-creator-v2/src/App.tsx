7686970612cf945adc1f706faa71274ef73b860d
TaehwanGo <gth1123@bubblecon.io>
2023-02-20 16:27:14 +0900

    BBC-932 v2 초기 세팅

--- packages/chai-contents-creator-v2/src/App.tsx
+++ packages/chai-contents-creator-v2/src/App.tsx
@@ -1,24 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { ExampleButton } from "chai-ui-v2";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>chai-creator</div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ExampleButton child={<div>차이홍 콘텐트 저작도구</div>} />
       </header>
     </div>
   );
