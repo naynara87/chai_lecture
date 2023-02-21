78719ef5941a163b2aafb53345b2eaad1bf11de4
1478952 <1478952@bubblecon.io>
2023-02-21 15:18:25 +0900

    BBC-975 모달 컴포넌트 구성

---
+++ packages/chai-contents-view-v2/src/components/modal/LayoutModalVoca.tsx
@@ -0,0 +1,61 @@
+import styled from "@emotion/styled";
+import React from "react";
+import ImgVoca from "../../images/img/img_voca.png";
+import IconSpeaker from "../../images/icon/icon_speaker_white.svg";
+import { ModalCommon } from "chai-ui-v2";
+import ComponentButtonFillBlack from "../molecules/ComponentButtonFillBlack";
+
+const BtnWrapper = styled.div`
+  .btn {
+    /* 28px */
+    font-size: 1.4vw;
+  }
+`;
+
+interface LayoutModalVocaProps {
+  isModalOpen: boolean;
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const LayoutModalVoca = ({
+  isModalOpen,
+  setIsModalOpen,
+}: LayoutModalVocaProps) => {
+  const handleClose = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    // NOTE: 설명 - active가 되면 보임
+    <ModalCommon open={isModalOpen} onClose={handleClose}>
+      {/* 제목영역 */}
+      <div className="base-ttl">
+        <div className="profile-img-wrap">
+          <img src={ImgVoca} alt="" className="img" />
+        </div>
+        <div className="txt-wrap">
+          <h2 className="ttl">새로운 단어</h2>
+        </div>
+      </div>
+      {/* 내용영역 */}
+      <div className="base-conts">
+        <div className="voca-wrap">
+          <p className="chinese">{"游泳"}</p>
+          <p className="pinyin">{"yóuyǒng"}</p>
+          <p className="mean">{"수영하다"}</p>
+        </div>
+        <button className="btn-icon">
+          <div className="icon-wrap">
+            <img src={IconSpeaker} alt="스피커모양" className="icon" />
+          </div>
+        </button>
+      </div>
+
+      <BtnWrapper className="btns-wrap">
+        <ComponentButtonFillBlack text="확인" />
+      </BtnWrapper>
+    </ModalCommon>
+  );
+};
+
+export default LayoutModalVoca;
---
+++ packages/chai-ui-v2/src/components/modal/index.ts
@@ -0,0 +1,1 @@
+export { default as ModalCommon } from "./ModalCommon";
