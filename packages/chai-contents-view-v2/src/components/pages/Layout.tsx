1b959c5f807b23ff409d96bfe7abaf2e6e8629cf
key <key9559@naver.com>
2023-02-21 16:41:38 +0900

    BBC-973 퀴즈 기본 형태 복붙

--- packages/chai-contents-view-v2/src/components/pages/Layout.tsx
+++ packages/chai-contents-view-v2/src/components/pages/Layout.tsx
@@ -4,9 +4,7 @@ import LayoutFooter from "../molecules/LayoutFooter";
 import Template01 from "./Template01";
 import Template02 from "./Template02";
 import Template03 from "./Template03";
-import TemplateDialogue from "./TemplateDialogue";
-import TemplateQuizDialogueWordBlank from "./TemplateQuizDialogueWordBlank";
-import TemplateQuizTextEnter from "./TemplateQuizTextEnter";
+import TemplateQuizRecord from "./TemplateQuizRecord";
 
 const Layout = () => {
 
@@ -14,7 +12,7 @@ const Layout = () => {
     <div>
       <LayoutHeader />
       <main className="cai-main">
-        <TemplateQuizTextEnter />
+        <TemplateQuizRecord />
       </main>
       <LayoutFooter />
     </div>
--- packages/chai-contents-view-v2/src/components/pages/TemplateQuizWordBlank.tsx
+++ packages/chai-contents-view-v2/src/components/pages/TemplateQuizWordBlank.tsx
@@ -1,6 +1,11 @@
+import styled from "@emotion/styled";
 import React from "react";
 import LayoutModal from "../molecules/LayoutModal";
 import LayoutModalVoca from "../molecules/LayoutModalVoca";
+import ComponentButtonFillBlack from "../molecules/ComponentButtonFillBlack";
+
+const QuizContainer = styled.form`
+`;
 
 const TemplateQuizWordBlank = () => {
 
@@ -10,11 +15,22 @@ const TemplateQuizWordBlank = () => {
         1234
       </div>
       <div className="layout-panel wide-panel">
-        asdf
+        <QuizContainer method="post" className="quiz-container">
+          <div className="quiz-question-wrap">
+            <p className="text">{`兴`}<span className="blank"></span>{`兴`}</p>
+            <p className="text-sm">{"기쁘다"}</p>
+          </div>
+          <div className="quiz-answer-wrap">
+            <input type="text" className="inp-txt" placeholder="빈칸에 들어갈 중국어를 입력하세요." />
+          </div>
+          <div className="btns-wrap">
+            <ComponentButtonFillBlack text="확인" />
+          </div>
+        </QuizContainer>
       </div>
       <LayoutModal />
       <LayoutModalVoca />
-    </div>
+    </div >
   );
 };
 
