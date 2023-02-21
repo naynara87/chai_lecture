f0210dc307f9d9695faa6a8c47fafb1dd0388ffd
key <key9559@naver.com>
2023-02-21 18:04:13 +0900

    BBC-973 퀴즈 -  회화 문장 빈칸채우기

--- packages/chai-contents-view-v2/src/components/pages/Layout.tsx
+++ packages/chai-contents-view-v2/src/components/pages/Layout.tsx
@@ -4,7 +4,7 @@ import LayoutFooter from "../molecules/LayoutFooter";
 import Template01 from "./Template01";
 import Template02 from "./Template02";
 import Template03 from "./Template03";
-import TemplateQuizDialogueChoice from "./TemplateQuizDialogueChoice";
+import TemplateQuizDialogueSentenceBlank from "./TemplateQuizDialogueSentenceBlank";
 
 const Layout = () => {
 
@@ -12,7 +12,7 @@ const Layout = () => {
     <div>
       <LayoutHeader />
       <main className="cai-main">
-        <TemplateQuizDialogueChoice />
+        <TemplateQuizDialogueSentenceBlank />
       </main>
       <LayoutFooter />
     </div>
