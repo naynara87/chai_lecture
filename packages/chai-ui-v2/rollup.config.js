4bd79aca363c372d68db2b926b816efb56801fe2
1478952 <1478952@bubblecon.io>
2023-02-22 16:49:53 +0900

    BBC-971 build 에러 수정

--- packages/chai-ui-v2/rollup.config.js
+++ packages/chai-ui-v2/rollup.config.js
@@ -1,7 +1,6 @@
 import Ts from "rollup-plugin-typescript2";
 import image from "@rollup/plugin-image";
 import scss from "rollup-plugin-scss";
-import url from "@rollup/plugin-url";
 import copy from "rollup-plugin-copy";
 
 export default {
@@ -13,16 +12,18 @@ export default {
     preserveModules: true, // preserve source folder
   },
   plugins: [
+    scss({
+      output: "./dist/globalStyle.css",
+      fileName: "globalStyle.css",
+      failOnError: true,
+    }),
     Ts(),
     image(),
-    scss({
-      include: ["/**/*.css", "/**/*.scss", "/**/*.sass"],
-      fileName: "globalStyle.css",
-    }),
-    url(),
     copy({
-      targets: [{ src: "src/fonts/", dest: "dist" }],
-      targets: [{ src: "src/images/", dest: "dist" }],
+      targets: [
+        { src: "src/fonts/", dest: "dist" },
+        { src: "src/images/", dest: "dist" },
+      ],
     }),
   ],
   external: [
