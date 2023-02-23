dd8de301389b1c11874a72d281c4e737a82618f0
1478952 <1478952@bubblecon.io>
2023-02-23 17:42:51 +0900

    BBC-982 colorPalette 적용

--- packages/chai-ui-v2/src/components/atoms/Button/StopButton.tsx
+++ packages/chai-ui-v2/src/components/atoms/Button/StopButton.tsx
@@ -1,11 +1,12 @@
 import styled from "@emotion/styled";
 import React from "react";
+import { colorPalette } from "../../../styles";
 import AudioStopIcon from "../AudioStopIcon";
 import IconButton from "./IconButton";
 
 // TODO: color palette 정의 후 적용하기 => BBC-982
 const CustomIconButton = styled(IconButton)`
-  border: 1px solid #e45757;
+  border: 1px solid ${colorPalette.red600};
   overflow: hidden;
 `;
 
--- packages/chai-ui-v2/src/index.ts
+++ packages/chai-ui-v2/src/index.ts
@@ -2,3 +2,4 @@ import "./styles/scss/ui.scss";
 
 export * from "./components";
 export * from "./core";
+export * from "./styles";
