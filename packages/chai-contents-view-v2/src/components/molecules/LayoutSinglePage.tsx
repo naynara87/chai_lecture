29e59b31985fb681ea1068428fc1202fd3e14e5f
1478952 <1478952@bubblecon.io>
2023-02-24 13:41:22 +0900

    BBC-996 템플릿 매퍼 수정

---
+++ packages/chai-contents-view-v2/src/components/molecules/LayoutSinglePage.tsx
@@ -0,0 +1,19 @@
+import { PageProps, SinglePage, useTemplateMapper } from "chai-ui-v2";
+import React from "react";
+
+interface LayoutSinglePageProps extends PageProps {}
+const LayoutSinglePage = ({
+  page,
+  setPageCompleted,
+}: LayoutSinglePageProps) => {
+  const singlePageData = page as SinglePage;
+
+  const { getTemplateComponent } = useTemplateMapper({
+    setPageCompleted,
+    page,
+  });
+
+  return <>{getTemplateComponent(singlePageData.data.type)}</>; // TODO: BBC-996
+};
+
+export default LayoutSinglePage;
