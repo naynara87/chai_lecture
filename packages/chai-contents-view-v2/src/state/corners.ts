e70efac74babee0342d658b54f9082a64ff46d9e
1478952 <1478952@bubblecon.io>
2023-02-27 19:50:46 +0900

    BBC-1005 코너완료시 다음 코너로 넘어가는 로직 추가

---
+++ packages/chai-contents-view-v2/src/state/corners.ts
@@ -0,0 +1,12 @@
+import { ID } from "chai-ui";
+import { atom } from "recoil";
+
+export type CornerCompleteState = {
+  id: ID;
+  isCompleted: boolean;
+};
+
+export const cornersState = atom<CornerCompleteState[]>({
+  key: "cornersState",
+  default: [],
+});
