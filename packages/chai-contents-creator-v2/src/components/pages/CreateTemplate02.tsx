5a3a9beffde65728aaae64fdc03fce1e105a2b4e
asoeg7315 <asoeg7315@bubblecon.io>
2023-02-28 18:12:44 +0900

    BBC-1007 styledComponents 분리작업

--- packages/chai-contents-creator-v2/src/components/pages/CreateTemplate02.tsx
+++ packages/chai-contents-creator-v2/src/components/pages/CreateTemplate02.tsx
@@ -1,165 +1,28 @@
-import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import {
+  CreateTemplateWrap,
+  CreateEditTop,
+  BtnChangeLayout,
+  BtnPreview,
+  BtnAddGanjiChange,
+  CreateEditMainWrap,
+  CreateEditMain,
+  DashBoxArea,
+  CreateTemplateChoiceBtnWrap,
+  CreateTemplateNavWrap,
+  CreateTemplateNav,
+  NavList,
+  CreateAddBtn,
+} from "../../styles/template";
 
-const buttonCSS = css`
-  background: white;
-  box-shadow: none;
-  border: 1px solid #d6d6d6;
-  padding: 8px 12px;
-  border-radius: 4px;
-  font-size: 12px;
-  font-weight: 500;
-  color: #565656;
-`;
-
-const CreateTemplateWrap = styled.div`
-  display: flex;
-  padding: 40px 24px;
-  flex-direction: column;
-  background-color: #f5f5f5;
-  height: 100vh;
-  & > div {
-    margin-bottom: 24px;
-  }
-`;
-
-const CreateEditTop = styled.div`
-  display: flex;
-  width: 100%;
-  margin-bottom: 16px !important;
-`;
-
-const BtnChangeLayout = styled.button`
-  ${buttonCSS}
-  margin-right: 8px;
-`;
-const BtnPreview = styled.button`
-  ${buttonCSS}
-`;
-const BtnAddGanjiChange = styled.button`
-  ${buttonCSS}
-  margin-left: auto;
-`;
-
-const CreateEditMainWrap = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const CreateTemplateNavWrap = styled.div`
-  text-align: left;
-  position: absolute;
-  height: 300px;
-  width: 280px;
-  overflow-y: scroll;
-  box-shadow: 2px 6px 12px #00000026;
-  padding: 16px;
-  right: 0;
-  transform: translateY(-100%);
-  background-color: #ffffff;
-  display: none;
-  transition: all 0.2s;
-
-  .nav-tit {
-    font-size: 12px;
-    font-weight: 500;
-    color: #565656;
-    margin-bottom: 10px;
-  }
-
-  &.active {
-    transform: translateY(0%);
-    display: block;
-  }
-`;
-
-const NavList = styled.ul`
-  .nav-li {
-    display: flex;
-    align-items: center;
-    margin-bottom: 8px;
-  }
-
-  .thumb-comp {
-    width: 50px;
-    height: 50px;
-    font-size: 12px;
-    font-weight: 700;
-    background-color: #d9d9d9;
-    line-height: 50px;
-    text-align: center;
-    border-radius: 4px;
-    margin-right: 8px;
-  }
-  .txt-comp {
-    font-size: 14px;
-    font-weight: 400;
-  }
-`;
-
-const CreateTemplateChoiceBtnWrap = styled.div`
-  margin-left: auto;
-  text-align: right;
-  position: relative;
-
-  .btn-comp-select {
-    margin-bottom: 8px;
-  }
-`;
-
-const DashBoxArea = styled.div`
-  width: 100%;
-  border: 2px dashed #888888;
-  padding: 16px;
-`;
-
-const CreateTemplateNav = styled.div`
-  margin-bottom: 14px;
-`;
-const CreateEditMain = styled.div`
-  display: flex;
-  padding: 24px;
-  box-sizing: border-box;
-  background-color: white;
-  height: 502px;
-  border-radius: 20px;
-  box-shadow: 0px 4px 19px rgba(145, 145, 145, 0.2);
-
-  &:nth-of-type(1) {
+const CreateEditMainWrap37 = styled(CreateEditMainWrap)`
+  & > div:nth-child(1) {
     width: 30%;
-    margin-right: 12px;
   }
-  &:nth-of-type(2) {
+  & > div:nth-child(2) {
     width: 70%;
-    margin-left: 12px;
-  }
-
-  & .btn-comp-select {
-    color: #7686d4;
-    background: #ffffff;
-    border-radius: 50px;
-    border: 1px solid #dbe1ff;
-    width: 90px;
-    height: 30px;
-    line-height: 30px;
-    font-size: 10px;
-    font-weight: 500;
   }
 `;
-
-const CreateAddBtn = styled.button`
-  background-color: #ffffff;
-  height: 102px;
-  box-shadow: 0px 4px 19px rgba(145, 145, 145, 0.2);
-  border-radius: 20px;
-  color: #888888;
-  font-size: 16px;
-  font-weight: 500;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
 const CreateTemplate02 = () => {
   return (
     <CreateTemplateWrap>
@@ -171,7 +34,7 @@ const CreateTemplate02 = () => {
         {/* TODO: lsh 버튼 클릭시 학습변경 간지 간지 추가 */}
         <BtnAddGanjiChange>학습 변경 간지 추가</BtnAddGanjiChange>
       </CreateEditTop>
-      <CreateEditMainWrap>
+      <CreateEditMainWrap37>
         <CreateEditMain>
           <DashBoxArea>
             <CreateTemplateChoiceBtnWrap>
@@ -249,7 +112,7 @@ const CreateTemplate02 = () => {
             </CreateTemplateChoiceBtnWrap>
           </DashBoxArea>
         </CreateEditMain>
-      </CreateEditMainWrap>
+      </CreateEditMainWrap37>
       {/*
       TODO: lsh 슬라이드 추가 버튼 클릭 시 레이아웃 선택화면으로 이동
       */}
