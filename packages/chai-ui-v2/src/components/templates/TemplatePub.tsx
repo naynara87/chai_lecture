fa7da6c94dab476651b103c6f6c6950d762b5c29
key <key9559@naver.com>
2023-03-02 10:40:40 +0900

    BBC-1000 스텝 문장카드

--- packages/chai-ui-v2/src/components/templates/TemplatePub.tsx
+++ packages/chai-ui-v2/src/components/templates/TemplatePub.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ComponentStepCard from "../molecules/ComponentStepCard";
+import ComponentStepSentenceCard from "../molecules/ComponentStepSentenceCard";
 import ComponentTitle from "../molecules/ComponentTitle";
 
 
@@ -8,7 +8,7 @@ const TemplatePub = () => {
     <div className="layout-panel-wrap">
       <div className="layout-panel">
         <ComponentTitle text="회화 속 주인공이 되어 말하기 연습을 해보세요." />
-        <ComponentStepCard />
+        <ComponentStepSentenceCard />
       </div>
     </div>
   );
