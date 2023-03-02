a5964b479b685f3bd9553e4d077811de70cdea18
1478952 <1478952@bubblecon.io>
2023-03-02 20:11:52 +0900

    BBC-1023 speaking 컴포넌트 오디오 추가

--- packages/chai-ui-v2/src/components/contents/SpeakingComponent.tsx
+++ packages/chai-ui-v2/src/components/contents/SpeakingComponent.tsx
@@ -1,5 +1,11 @@
 import styled from "@emotion/styled";
-import React, { useCallback, useMemo, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { SpeakingContentData } from "../../core";
 import ComponentButtonFillBlackMini from "../atoms/ComponentButtonFillBlackMini";
 import ComponentProgress from "../atoms/ComponentProgress";
@@ -12,15 +18,25 @@ interface SpeakingComponentProps {
 }
 
 const SpeakingComponent = ({ contents }: SpeakingComponentProps) => {
+  const audioRef = useRef<HTMLAudioElement>(null);
   const [isShowProgressBar, setIsShowProgressBar] = useState(false);
   const [isEndProgressBar, setIsEndProgressBar] = useState(false);
+  const [isAudioEnd, setIsAudioEnd] = useState(false);
+
+  useEffect(() => {
+    audioRef.current?.addEventListener("ended", () => {
+      setIsAudioEnd(true);
+      setTimeout(() => {
+        setIsEndProgressBar(true);
+      }, contents.data.speakingTime * 1000);
+    });
+  }, [contents.data.speakingTime]);
 
   const handleClickStartButton = useCallback(() => {
+    if (!audioRef.current) return;
     setIsShowProgressBar(true);
-    setTimeout(() => {
-      setIsEndProgressBar(true);
-    }, contents.data.speakingTime * 1000);
-  }, [contents.data.speakingTime]);
+    void audioRef.current.play();
+  }, []);
 
   const repeatSpeak = useMemo(() => {
     if (isShowProgressBar && isEndProgressBar) {
@@ -36,7 +52,10 @@ const SpeakingComponent = ({ contents }: SpeakingComponentProps) => {
           <div className="text-wrap">
             <span className="text">잘 듣고 따라 말해보세요!</span>
           </div>
-          <ComponentProgress progressDuration={contents.data.speakingTime} />
+          <ComponentProgress
+            progressDuration={contents.data.speakingTime}
+            isAudioEnd={isAudioEnd}
+          />
         </>
       );
     } else if (!isShowProgressBar && !isEndProgressBar) {
@@ -57,10 +76,16 @@ const SpeakingComponent = ({ contents }: SpeakingComponentProps) => {
     handleClickStartButton,
     isEndProgressBar,
     isShowProgressBar,
+    isAudioEnd,
   ]);
 
   return (
-    <RepeatSpeak className="repeat-speak-wrapper">{repeatSpeak}</RepeatSpeak>
+    <RepeatSpeak className="repeat-speak-wrapper">
+      <audio ref={audioRef}>
+        <source src={contents.data.src} />
+      </audio>
+      {repeatSpeak}
+    </RepeatSpeak>
   );
 };
 
