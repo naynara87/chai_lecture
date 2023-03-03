3ae9f341abc9ae115549d169923f8ef0f0f505f1
key <key9559@naver.com>
2023-03-03 13:56:35 +0900

    BBC-1000 7-문화-4 리스트 이미지

--- packages/chai-ui-v2/src/components/templates/TemplatePub.tsx
+++ packages/chai-ui-v2/src/components/templates/TemplatePub.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import ComponentImageMultiCaption from "../molecules/ComponentImageMultiCaption";
+import { ComponentContsInfo } from "../molecules";
+import ComponentCultureList from "../molecules/ComponentCultureList";
 import ComponentText from "../molecules/ComponentText";
 
 const TemplatePub = () => {
   return (
-    <div className="layout-panel-wrap">
-      <div className="layout-panel">
+    <div className="layout-panel-wrap grid-h-3-7">
+      <div className="layout-panel side-panel">
+        <ComponentContsInfo text="명칭의 유래" />
+      </div>
+      <div className="layout-panel wide-panel">
         <ComponentText />
-        <ComponentImageMultiCaption />
-        <ComponentText />
+        <ComponentCultureList />
       </div>
     </div>
   );
