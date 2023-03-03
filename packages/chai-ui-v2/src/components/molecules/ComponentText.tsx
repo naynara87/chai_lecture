475cd91f994a80608b2351451bd8e650ed2762ce
key <key9559@naver.com>
2023-03-03 12:26:22 +0900

    BBC-1000 7-문화-3

--- packages/chai-ui-v2/src/components/molecules/ComponentText.tsx
+++ packages/chai-ui-v2/src/components/molecules/ComponentText.tsx
@@ -35,7 +35,7 @@ const ComponentText = () => {
       <p className="text">{`Shǒu'ěr de`}<b>{`dōngtiān`}</b>{`méiyǒu Běijīng lěng`}</p>
       <p className="text">{`서울의 겨울은 베이징보다 춥지 않아요.`}</p>
       <p className="text bold-ttl">{`베이징 오리구이`}</p>
-      <p className="text bold">{`정도부사의 종류​`}</p>
+      <p className="text bold">{`정도부사의 종류`}</p>
       <p className="text">{`베이징 오리구이(北京烤鸭)란?`}</p>
     </TextComponent>
   );
