846daeac98e7d4b200c25c8c3cc691e6f54a68f1
key <key9559@naver.com>
2023-03-03 18:03:27 +0900

    BBC-1000 학습정리

--- packages/chai-ui-v2/src/components/templates/TemplatePub.tsx
+++ packages/chai-ui-v2/src/components/templates/TemplatePub.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { ComponentVocaNote } from "../molecules";
-import ComponentCultureToggleText from "../molecules/ComponentCultureToggleText";
+import { ComponentTitle } from "../molecules";
+import ComponentTraining from "../molecules/ComponentTraining";
 
 const TemplatePub = () => {
   return (
-    <div className="layout-panel-wrap grid-h-3-7">
-      <div className="layout-panel side-panel">
-        <ComponentVocaNote />
-      </div>
-      <div className="layout-panel wide-panel">
-        <ComponentCultureToggleText />
+    <div className="layout-panel-wrap">
+      <div className="layout-panel">
+        <ComponentTitle text="이번 레슨에서 배운 내용을 확인해볼까요?" />
+        <ComponentTraining />
       </div>
     </div>
   );
