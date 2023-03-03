6c24b24881d96a4a2c012db14443877d9d1c4002
key <key9559@naver.com>
2023-03-03 10:12:08 +0900

    BBC-1000 탭

--- packages/chai-ui-v2/src/components/templates/TemplatePub.tsx
+++ packages/chai-ui-v2/src/components/templates/TemplatePub.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { ComponentContsInfo } from "../molecules";
-import ComponentStepCard from "../molecules/ComponentStepCard";
+import ComponentTitle from "../molecules/ComponentTitle";
+import ComponentImage from "../molecules/ComponentImage";
+import ComponentText from "../molecules/ComponentText";
+import ComponentTabContents from "../molecules/ComponentTabContents";
 
 
 const TemplatePub = () => {
   return (
     <div className="layout-panel-wrap grid-h-5-5">
       <div className="layout-panel side-panel">
-        <ComponentContsInfo text="이벤엔 중국 문화에 대해 알아볼 차례예요!
-빈 칸에 들어갈 말이 무엇일지 생각해볼까요?" />
+        <ComponentImage />
+        <ComponentTitle text="정도부사에 대해 알아봅시다." />
+        <ComponentText />
       </div>
       <div className="layout-panel wide-panel">
-        <ComponentStepCard />
+        <ComponentTabContents />
       </div>
     </div>
   );
