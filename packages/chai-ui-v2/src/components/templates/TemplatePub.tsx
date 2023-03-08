d657b0869e2dc40b8aa9a86611916d1a7bed3107
key <key9559@naver.com>
2023-03-08 14:12:45 +0900

    BBC-1040 영상 컴포넌트 상단 여백 삭제

--- packages/chai-ui-v2/src/components/templates/TemplatePub.tsx
+++ packages/chai-ui-v2/src/components/templates/TemplatePub.tsx
@@ -2,16 +2,12 @@ import React from "react";
 import { ComponentButtonRadiBorderMain, ComponentButtonRadiFillMain } from "../atoms";
 import IconDictionaryButton from "../atoms/Button/IconDictionaryButton";
 import IconPlayButton from "../atoms/Button/IconPlayButton";
-import ComponentImage from "../molecules/ComponentImage";
-import ComponentText from "../molecules/ComponentText";
 
 const TemplatePub = () => {
 
   return (
     <div className="layout-panel-wrap">
       <div className="layout-panel">
-        <ComponentImage />
-        <ComponentText />
         <div className="btns-wrap">
           <IconPlayButton active={true} />
           <IconDictionaryButton />
