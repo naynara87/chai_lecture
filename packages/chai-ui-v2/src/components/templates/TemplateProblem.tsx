9e276059677c7e3a9e9dfd99caf0f587c7966959
key <key9559@naver.com>
2023-03-09 16:44:11 +0900

    BBC-1063 상단 버튼 영역

--- packages/chai-ui-v2/src/components/templates/TemplateProblem.tsx
+++ packages/chai-ui-v2/src/components/templates/TemplateProblem.tsx
@@ -1,28 +1,28 @@
 import React from "react";
 import { ComponentButtonRadiFillMain } from "../atoms";
-import ComponentProblemPagination from "../molecules/ComponentProblemPagination";
+import ComponentProblemTopButtonArea from "../molecules/ComponentProblemTopButtonArea";
 
 const TemplateProblem = () => {
   return (
     <>
-      {/* test header */}
-      <header className="cai-hd test-hd">
+      {/* problem header */}
+      <header className="cai-hd problem-hd">
         <div className="hd-conts-wrap">
-          <h1 className="test-hd-ttl">{'빨강 연습문제'}</h1>
-          <div className="test-hd-timer">{'00:00:00'}</div>
+          <h1 className="problem-hd-ttl">{'빨강 연습문제'}</h1>
+          <div className="problem-hd-timer">{'00:00:00'}</div>
         </div>
       </header>
-      {/* end test header */}
-      <ComponentProblemPagination />
-      {/* test main layout */}
-      <main className="cai-main test-main">
+      {/* end problem header */}
+      {/* problem main layout */}
+      <main className="cai-main problem-main">
+        <ComponentProblemTopButtonArea />
         <div className="layout-panel-wrap">
           <div className="layout-panel">
           </div>
         </div>
         <ComponentButtonRadiFillMain text="채점하기" />
       </main>
-      {/* end test main layout */}
+      {/* end problem main layout */}
     </>
   );
 };
