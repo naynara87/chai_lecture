eca44fd87e419e6a42948178245a99cddfb54539
Tony <gth1123@naver.com>
2023-03-11 00:15:17 +0900

    BBC-1081 focus on editor mount

--- packages/chai-contents-creator-v2/src/components/atoms/TextEditor.tsx
+++ packages/chai-contents-creator-v2/src/components/atoms/TextEditor.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
@@ -7,6 +7,13 @@ interface TextEditorProps {
   setText: (text: string) => void;
 }
 const TextEditor = ({ text, setText }: TextEditorProps) => {
+  useEffect(() => {
+    const quill = document.querySelector<HTMLDivElement>(".ql-editor");
+    if (quill) {
+      quill.focus();
+    }
+  }, []);
+
   const handleChange = (value: string) => {
     // todo : 컴포넌트 데이터 변경하기 - debounce 적용 고려?
     setText(value);
