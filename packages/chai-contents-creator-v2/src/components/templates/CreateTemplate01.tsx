3c4005574d21a7ff68566c42511971091c332c2a
asoeg7315 <asoeg7315@bubblecon.io>
2023-03-13 15:46:01 +0900

    BBC-1101 AudioRecorderCreator 저작화면 퍼블리싱

--- packages/chai-contents-creator-v2/src/components/templates/CreateTemplate01.tsx
+++ packages/chai-contents-creator-v2/src/components/templates/CreateTemplate01.tsx
@@ -8,7 +8,7 @@ import {
 } from "../../styles/template";
 import { PageCommonProps } from "../../types/page";
 import DashBoxArea from "../atoms/DashBoxArea";
-import SpeakingCreator from "../contents/SpeakingCreator";
+import AudioRecorderCreator from "../contents/AudioRecorderCreator";
 import PageHeader from "../molecules/PageHeader";
 
 const CreateTemplate01 = ({
@@ -62,7 +62,7 @@ const CreateTemplate01 = ({
                 position: "contents",
               });
             })}
-            <SpeakingCreator />
+            <AudioRecorderCreator />
           </DashBoxArea>
         </CreateEditMain>
       </CreateEditMainWrap>
