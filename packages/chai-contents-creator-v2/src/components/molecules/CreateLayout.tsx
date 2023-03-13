fa6e9128a63e3c87d35b412f8b5b56c6e23a9c84
key <key9559@naver.com>
2023-03-13 11:45:59 +0900

    BBC-1085 모달 레이아웃 선택창

--- packages/chai-contents-creator-v2/src/components/molecules/CreateLayout.tsx
+++ packages/chai-contents-creator-v2/src/components/molecules/CreateLayout.tsx
@@ -9,67 +9,73 @@ import {
 import { colorPalette, TemplateType } from "chai-ui-v2";
 
 const Header = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  padding: 20px;
+  padding-bottom: 0;
+  text-align: center;
 `;
 
 const HeaderTitle = styled.h1`
-  font-size: 24px;
+  font-size: 30px;
   font-weight: 700;
-  line-height: 35px;
+  line-height: 1.5;
 `;
 
-const HeaderSubTitle = styled.h4`
-  margin-top: 16px;
-  font-size: 16px;
-  font-weight: 500;
-  line-height: 24px;
+const HeaderSubTitle = styled.p`
+  margin-top: 10px;
+  font-size: 24px;
+  line-height: 1.5;
 `;
 
 const Main = styled.main`
-  padding: 40px 0px;
+  padding: 0 20px 35px;
 `;
 
 const MainSection = styled.section`
-  margin-top: 48px;
-  :first-of-type {
-    margin-top: 8px;
-  }
+  margin-top: 35px;
 `;
 
 const MainSectionTitle = styled.h2`
-  font-size: 16px;
-  font-weight: 500;
-  line-height: 24px;
-  margin-bottom: 8px;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 1.6;
 `;
 
 const LayoutContainer = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  gap: 24px;
+  gap: 15px 20px;
+  margin-top: 10px;
 `;
 
 const LayoutItem = styled.li`
-  align-items: center;
-  width: calc(25% - 24px);
+  width: calc(25% - 15px);
   cursor: pointer;
   text-align: center;
+
+  > img {
+    width: 100%;
+  }
+
+  @media all and (max-width: 768px) {
+    width: calc(33% - 15px);
+  }
+
+  @media all and (max-width: 600px) {
+    width: calc(50% - 10px);
+  }
 `;
 
 const LayoutItemName = styled.div`
-  font-size: 20px;
+  font-size: 15px;
   font-weight: 500;
-  line-height: 30px;
-  margin-top: 8px;
+  line-height: 1.3;
   word-break: keep-all;
 `;
 
 const ChooseLayoutWrapper = styled.div`
-  background-color: ${colorPalette.white};
-  height: 70vh;
   overflow: auto;
+  max-height: 70vh;
+  background-color: ${colorPalette.white};
 `;
 
 interface ChooseLayoutProps {
