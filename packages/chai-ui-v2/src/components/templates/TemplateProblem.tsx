f2dfd0222b655516cd435478d6326ee6c6d5009d
key <key9559@naver.com>
2023-03-20 17:28:43 +0900

    BBC-1129 문항번호 영역 추가, iframe 들어갈 위치 잡음

--- packages/chai-ui-v2/src/components/templates/TemplateProblem.tsx
+++ packages/chai-ui-v2/src/components/templates/TemplateProblem.tsx
@@ -1,9 +1,17 @@
+import styled from "@emotion/styled";
 import React from "react";
-import { ComponentButtonRadiFillMain } from "../atoms";
-import ComponentProblemCommentary from "../molecules/ComponentProblemCommentary";
-import ComponentProblemGrade from "../molecules/ComponentProblemGrade";
-import ComponentProblemTopButtonArea from "../molecules/ComponentProblemTopButtonArea";
-import ComponentProblemUserInfo from "../molecules/ComponentProblemUserInfo";
+import BtnIcon from "../../assets/images/icon/icon_stick_arrow_right_white.svg";
+
+const IframeWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background: red;
+
+  iframe {
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 const TemplateProblem = () => {
   return (
@@ -12,7 +20,7 @@ const TemplateProblem = () => {
 
       {/* end problem header */}
       {/* problem main layout */}
-      <main className="cai-main problem-main">
+      {/* <main className="cai-main problem-main">
         <ComponentProblemTopButtonArea />
         <div className="layout-panel-wrap grid-h-3-7">
           <div className="layout-panel">
@@ -24,6 +32,37 @@ const TemplateProblem = () => {
           </div>
         </div>
         <ComponentButtonRadiFillMain text="채점하기" />
+      </main>
+       */}
+
+
+      <header className="cai-hd problem-hd">
+        <div className="hd-conts-wrap">
+          <h1 className="problem-hd-ttl">{'빨강 연습문제'}</h1>
+          <div className="problem-hd-timer">{'00:00:00'}</div>
+        </div>
+      </header>
+      <main className="cai-main problem-main">
+        <div className="problem-top-button-wrapper pagination-wrapper">
+          {/* TODO: 설명 - 열면 클래스 active 추가, 문제풀면 end 추가 */}
+          <button className="problem-pagination end">{'1'}</button>
+          <button className="problem-pagination active end">{'92'}</button>
+          <button className="problem-pagination active">{'993'}</button>
+          <button className="problem-pagination">{'9994'}</button>
+        </div>
+        <div className="layout-panel-wrap">
+          {/* 문항번호 */}
+          <div className="question-number">{`[50 ~ 87]
+        55번`}</div>
+          {/* end 문항번호 */}
+          <div className="layout-panel">
+            <IframeWrapper>문제 들어오는 영역</IframeWrapper>
+          </div>
+          <button className="btn btn-problem">
+            <span>채점하기</span>
+            <img src={BtnIcon} alt="바로가기아이콘" />
+          </button>
+        </div>
       </main>
       {/* end problem main layout */}
     </>
