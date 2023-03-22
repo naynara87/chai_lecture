2820a5a6f66454468d95346c3914192c35d0fb2a
key <key9559@naver.com>
2023-03-22 17:30:02 +0900

    BBC-1129 문제템플릿 해석해설 화면 변경된 스타일 적용

--- packages/chai-ui-v2/src/components/molecules/ComponentProblemCommentary.tsx
+++ packages/chai-ui-v2/src/components/molecules/ComponentProblemCommentary.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import IconEmpty from "../../assets/images/icon/icon_empty.svg";
-import IconRight from "../../assets/images/icon/icon_problem_o.svg";
-// import IconWrong from "../../assets/images/icon/icon_problem_x.svg";
+// import IconRight from "../../assets/images/icon/icon_problem_o.svg";
+import IconWrong from "../../assets/images/icon/icon_problem_x.svg";
 
 const ComponentProblemCommentary = () => {
   return (
     <div className="problem-commentary-wrapper">
       {/* TODO: key설명 - 항목 선택을 하지 않았을 때 */}
-      <div className="empty-conts">
+      <div className="empty-conts none">
         <img src={IconEmpty} alt="없음" className="img" />
         <h3 className="emp-ttl">채점표에서 번호를 <br />선택해주세요</h3>
         <p className="emp-txt">여기에 정답과 해설이 표시됩니다.</p>
       </div>
 
       {/* TODO: key설명 - 항목선택을 했을 때 */}
-      <div className="problem-commentary-wrap none">
-        <h3 className="com-ttl"><b>{`Q1`}</b> <img src={IconRight} alt="정답" /></h3>
+      <div className="problem-commentary-wrap">
+        <h3 className="com-ttl"><b>{`1번`}</b> <img src={IconWrong} alt="오답" /></h3>
         <div className="tab-iframe-wrap">
           {/* TODO: key설명 - 버튼을 누르면 본인에 클래스active 추가,
            tab-conts-wrapper에 클래스 none 삭제 */}
