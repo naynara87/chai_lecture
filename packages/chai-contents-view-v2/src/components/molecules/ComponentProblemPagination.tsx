b9e0e469204f50bda2caf69634f013fb6ffb4869
asoeg7315 <asoeg7315@bubblecon.io>
2023-03-27 18:56:10 +0900

    BBC-1176 문제템플릿 상단 페이지베이션 드래그 기능구현

--- packages/chai-contents-view-v2/src/components/molecules/ComponentProblemPagination.tsx
+++ packages/chai-contents-view-v2/src/components/molecules/ComponentProblemPagination.tsx
@@ -1,11 +1,26 @@
 import { LocalStorage, Page, QuizData } from "chai-ui-v2";
 import React, { useMemo } from "react";
 import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import styled from "@emotion/styled";
 
 interface ComponentProblemPaginationProps {
   pages: Page[];
   onClickPagination: (pageIndex: number) => void;
 }
+
+const SwiperWrapper = styled.div`
+  position: relative;
+
+  .swiper-wrapper {
+    padding: 14px 0;
+  }
+  .swiper-slide:not(.swiper-slide-active) {
+    opacity: 1;
+  }
+`;
 
 const ComponentProblemPagination = ({
   pages,
@@ -18,23 +33,35 @@ const ComponentProblemPagination = ({
     const questionDatas = LocalStorage.getItem("pageData") as QuizData[];
     return pages.map((page, pageIndex) => {
       return (
-        <button
-          className={`problem-pagination ${
-            questionDatas ? questionDatas[pageIndex].state : ""
-          } ${(pageIndex + 1).toString() === pageId ? "active" : ""} `}
-          key={pageIndex}
-          onClick={() => onClickPagination(pageIndex)}
-        >
-          {pageIndex + 1}
-        </button>
+        <SwiperSlide key={pageIndex}>
+          <button
+            className={`problem-pagination ${
+              questionDatas ? questionDatas[pageIndex].state : ""
+            } ${(pageIndex + 1).toString() === pageId ? "active" : ""} `}
+            key={pageIndex}
+            onClick={() => onClickPagination(pageIndex)}
+          >
+            {pageIndex + 1}
+          </button>
+        </SwiperSlide>
       );
     });
   }, [pages, onClickPagination, pageId]);
 
   return (
-    <div className="problem-top-button-wrapper pagination-wrapper">
-      {pagination}
-    </div>
+    <SwiperWrapper>
+      <Swiper
+        loop={false}
+        pagination={false}
+        spaceBetween={16}
+        slidesPerView={18}
+        slidesPerGroup={1}
+        scrollbar={{ draggable: true }}
+        className="problem-top-button-wrapper pagination-wrapper"
+      >
+        {pagination}
+      </Swiper>
+    </SwiperWrapper>
   );
 };
 
