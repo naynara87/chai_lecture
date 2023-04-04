435706a0fbe8d4c291b806f10e150a126aa0f936
key <key9559@naver.com>
2023-04-04 10:15:35 +0900

    BBC-1292 QA39 에디터로 입력받을 때 에디터 태그 여백 삭제

--- packages/chai-ui-v2/src/components/atoms/HtmlContentComponent.tsx
+++ packages/chai-ui-v2/src/components/atoms/HtmlContentComponent.tsx
@@ -1,7 +1,15 @@
 import styled from "@emotion/styled";
 import React from "react";
 
-export const HtmlDiv = styled.div``;
+export const HtmlDiv = styled.div`
+  h1,h2,h3,h4,h5,h6,p,span {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+    line-height: 1.6;
+  }
+`;
 
 interface HtmlContentComponentProps {
   html: string;
