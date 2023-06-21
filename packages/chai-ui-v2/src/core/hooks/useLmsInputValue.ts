99ea3d76fcc0dd3c15d8da94fa92a69df648af5d
1478952 <1478952@bubblecon.io>
2023-06-21 10:37:27 +0900

    BBC-1705 음성에세이 제출버튼 클릭시 팝업 생성

--- packages/chai-ui-v2/src/core/hooks/useLmsInputValue.ts
+++ packages/chai-ui-v2/src/core/hooks/useLmsInputValue.ts
@@ -33,9 +33,9 @@ interface InitialAppData {
 }
 
 const getTestData = (): InitialAppData => {
-  const lessonId = 329;
-  // const lessonId = 418;
-  const isContents = false; // 콘텐츠 true, 문제 false
+  // const lessonId = 329;
+  const lessonId = 418;
+  // const isContents = false; // 콘텐츠 true, 문제 false
 
   return {
     uno: "1",
@@ -45,8 +45,8 @@ const getTestData = (): InitialAppData => {
     courseId: "1",
     courseName: "red",
     subjectId: "1",
-    lessonId: "418", // 문제
-    // lessonId, // 콘텐츠
+    // lessonId: "418", // 문제
+    lessonId, // 콘텐츠
     // lessonId: "12345",
     lessonName: "red1",
     // turnId: "808e1d4a-725c-45a4-bef7-ab01aae81103",
@@ -77,152 +77,152 @@ const getTestData = (): InitialAppData => {
     //   "https://profile.caihong.co.kr/content-management/course/lessons": null,
     //   "https://profile.caihong.co.kr/content-management/course/lessons/lesson": null,
     // },
-    object_context: {
-      "https://profile.caihong.co.kr/content-management/course/local-content-id":
-        lessonId, // 레슨 id
-      "https://profile.caihong.co.kr/content-management/course/course-id": 1,
-      "https://profile.caihong.co.kr/content-management/course/course-name":
-        "개발콘텐츠",
-      "https://profile.caihong.co.kr/content-management/course/course-name/chinese":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/course-name/english":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/course-name/spanish":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/course-type-id": 10,
-      "https://profile.caihong.co.kr/content-management/course/course-type-name":
-        "빨강",
-      "https://profile.caihong.co.kr/content-management/course/image-id": null,
-      "https://profile.caihong.co.kr/content-management/course/image-url": null,
-      "https://profile.caihong.co.kr/content-management/course/lessons/lesson-name":
-        "1레슨",
-      "https://profile.caihong.co.kr/content-management/course/lessons/lesson-id":
-        lessonId,
-      "https://profile.caihong.co.kr/content-management/course/lessons/lesson-type-code":
-        isContents ? 10 : 20,
-      "https://profile.caihong.co.kr/content-management/course/lessons": null,
-      "https://profile.caihong.co.kr/content-management/course/lessons/lesson":
-        null,
-    },
-    context_details: {
-      registration: "cf3ca788-d7cc-464e-a654-a98a777d2372",
-      revision: "1.0",
-      contextActivities: {
-        category: [
-          {
-            id: "https://profile.caihong.co.kr/xapi/profile/course",
-            objectType: "Activity",
-          },
-        ],
-        parent: [
-          {
-            definition: {
-              description: {
-                "en-US": "과정 설명",
-              },
-              name: {
-                "en-US": "과정명",
-              },
-            },
-            id: "https://profile.caihong.co.kr/content-management/course/",
-            objectType: "Activity",
-          },
-        ],
-      },
-    },
-    extension_details: {
-      "https://profile.caihong.co.kr/content-management/course/subject-name":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/subject-id":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/curriculum-name":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/curriculum-id":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/recommendage-name":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/recommendage-id":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/learning-type-name":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/learning-type-id":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/difficulty-name":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/difficulty-id":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/year": null,
-      "https://profile.caihong.co.kr/content-management/course/rating-name":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/rating-id": null,
-      "https://profile.caihong.co.kr/content-management/course/publiccourse-name":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/publiccourse-id":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/registration-date":
-        null,
-      "https://profile.caihong.co.kr/course-management/course/education-type-name":
-        "디지털",
-      "https://profile.caihong.co.kr/course-management/course/education-type-code":
-        "ET01",
-      "https://profile.caihong.co.kr/course-management/course/course-name":
-        "[디지털] 과정 101",
-      "https://profile.caihong.co.kr/course-management/course/course-name-id": 101,
-      "https://profile.caihong.co.kr/course-management/course/course-code":
-        "CSR101",
-      "https://profile.caihong.co.kr/course-management/course/classification-name":
-        null,
-      "https://profile.caihong.co.kr/course-management/course/classification-code":
-        null,
-      "https://profile.caihong.co.kr/course-management/course/category-name":
-        null,
-      "https://profile.caihong.co.kr/course-management/course/category-code":
-        null,
-      "https://profile.caihong.co.kr/course-management/course/recommendage-name":
-        null,
-      "https://profile.caihong.co.kr/course-management/course/recommendage-code":
-        null,
-      "https://profile.caihong.co.kr/course-management/course/period": null,
-      "https://profile.caihong.co.kr/course-management/course/use": null,
-      "https://profile.caihong.co.kr/course-management/course/registration-date":
-        null,
-      "https://profile.caihong.co.kr/product-management/course/product-code":
-        "CH00241",
-      "https://profile.caihong.co.kr/product-management/course/producttype-code":
-        "PGC01",
-      "https://profile.caihong.co.kr/product-management/course/classification-name":
-        "주니어",
-      "https://profile.caihong.co.kr/product-management/course/classification-code":
-        "B001",
-      "https://profile.caihong.co.kr/product-management/course/category-name":
-        null,
-      "https://profile.caihong.co.kr/product-management/course/category-code":
-        null,
-      "https://profile.caihong.co.kr/product-management/course/education-type-name":
-        "방문",
-      "https://profile.caihong.co.kr/product-management/course/education-type-code":
-        "ET01",
-      "https://profile.caihong.co.kr/product-management/course/recommendage-name":
-        null,
-      "https://profile.caihong.co.kr/product-management/course/recommendage-code":
-        null,
-      "https://profile.caihong.co.kr/product-management/course/product-name":
-        "[결합] 학습용 결합상품 01",
-      "https://profile.caihong.co.kr/product-management/course/product-id": 42,
-      "https://profile.caihong.co.kr/product-management/course/pay": null,
-      "https://profile.caihong.co.kr/product-management/course/period-name":
-        null,
-      "https://profile.caihong.co.kr/product-management/course/period-code":
-        null,
-      "https://profile.caihong.co.kr/product-management/course/display": null,
-      "https://profile.caihong.co.kr/product-management/course/sale": null,
-      "https://profile.caihong.co.kr/product-management/course/price": null,
-      "https://profile.caihong.co.kr/product-management/course/review-period":
-        null,
-      "https://profile.caihong.co.kr/product-management/course/registration-date":
-        null,
-      "https://profile.caihong.co.kr/content-management/course/lessons/pages": 20,
-    },
+    // object_context: {
+    //   "https://profile.caihong.co.kr/content-management/course/local-content-id":
+    //     lessonId, // 레슨 id
+    //   "https://profile.caihong.co.kr/content-management/course/course-id": 1,
+    //   "https://profile.caihong.co.kr/content-management/course/course-name":
+    //     "개발콘텐츠",
+    //   "https://profile.caihong.co.kr/content-management/course/course-name/chinese":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/course-name/english":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/course-name/spanish":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/course-type-id": 10,
+    //   "https://profile.caihong.co.kr/content-management/course/course-type-name":
+    //     "빨강",
+    //   "https://profile.caihong.co.kr/content-management/course/image-id": null,
+    //   "https://profile.caihong.co.kr/content-management/course/image-url": null,
+    //   "https://profile.caihong.co.kr/content-management/course/lessons/lesson-name":
+    //     "1레슨",
+    //   "https://profile.caihong.co.kr/content-management/course/lessons/lesson-id":
+    //     lessonId,
+    //   "https://profile.caihong.co.kr/content-management/course/lessons/lesson-type-code":
+    //     isContents ? 10 : 20,
+    //   "https://profile.caihong.co.kr/content-management/course/lessons": null,
+    //   "https://profile.caihong.co.kr/content-management/course/lessons/lesson":
+    //     null,
+    // },
+    // context_details: {
+    //   registration: "cf3ca788-d7cc-464e-a654-a98a777d2372",
+    //   revision: "1.0",
+    //   contextActivities: {
+    //     category: [
+    //       {
+    //         id: "https://profile.caihong.co.kr/xapi/profile/course",
+    //         objectType: "Activity",
+    //       },
+    //     ],
+    //     parent: [
+    //       {
+    //         definition: {
+    //           description: {
+    //             "en-US": "과정 설명",
+    //           },
+    //           name: {
+    //             "en-US": "과정명",
+    //           },
+    //         },
+    //         id: "https://profile.caihong.co.kr/content-management/course/",
+    //         objectType: "Activity",
+    //       },
+    //     ],
+    //   },
+    // },
+    // extension_details: {
+    //   "https://profile.caihong.co.kr/content-management/course/subject-name":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/subject-id":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/curriculum-name":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/curriculum-id":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/recommendage-name":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/recommendage-id":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/learning-type-name":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/learning-type-id":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/difficulty-name":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/difficulty-id":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/year": null,
+    //   "https://profile.caihong.co.kr/content-management/course/rating-name":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/rating-id": null,
+    //   "https://profile.caihong.co.kr/content-management/course/publiccourse-name":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/publiccourse-id":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/registration-date":
+    //     null,
+    //   "https://profile.caihong.co.kr/course-management/course/education-type-name":
+    //     "디지털",
+    //   "https://profile.caihong.co.kr/course-management/course/education-type-code":
+    //     "ET01",
+    //   "https://profile.caihong.co.kr/course-management/course/course-name":
+    //     "[디지털] 과정 101",
+    //   "https://profile.caihong.co.kr/course-management/course/course-name-id": 101,
+    //   "https://profile.caihong.co.kr/course-management/course/course-code":
+    //     "CSR101",
+    //   "https://profile.caihong.co.kr/course-management/course/classification-name":
+    //     null,
+    //   "https://profile.caihong.co.kr/course-management/course/classification-code":
+    //     null,
+    //   "https://profile.caihong.co.kr/course-management/course/category-name":
+    //     null,
+    //   "https://profile.caihong.co.kr/course-management/course/category-code":
+    //     null,
+    //   "https://profile.caihong.co.kr/course-management/course/recommendage-name":
+    //     null,
+    //   "https://profile.caihong.co.kr/course-management/course/recommendage-code":
+    //     null,
+    //   "https://profile.caihong.co.kr/course-management/course/period": null,
+    //   "https://profile.caihong.co.kr/course-management/course/use": null,
+    //   "https://profile.caihong.co.kr/course-management/course/registration-date":
+    //     null,
+    //   "https://profile.caihong.co.kr/product-management/course/product-code":
+    //     "CH00241",
+    //   "https://profile.caihong.co.kr/product-management/course/producttype-code":
+    //     "PGC01",
+    //   "https://profile.caihong.co.kr/product-management/course/classification-name":
+    //     "주니어",
+    //   "https://profile.caihong.co.kr/product-management/course/classification-code":
+    //     "B001",
+    //   "https://profile.caihong.co.kr/product-management/course/category-name":
+    //     null,
+    //   "https://profile.caihong.co.kr/product-management/course/category-code":
+    //     null,
+    //   "https://profile.caihong.co.kr/product-management/course/education-type-name":
+    //     "방문",
+    //   "https://profile.caihong.co.kr/product-management/course/education-type-code":
+    //     "ET01",
+    //   "https://profile.caihong.co.kr/product-management/course/recommendage-name":
+    //     null,
+    //   "https://profile.caihong.co.kr/product-management/course/recommendage-code":
+    //     null,
+    //   "https://profile.caihong.co.kr/product-management/course/product-name":
+    //     "[결합] 학습용 결합상품 01",
+    //   "https://profile.caihong.co.kr/product-management/course/product-id": 42,
+    //   "https://profile.caihong.co.kr/product-management/course/pay": null,
+    //   "https://profile.caihong.co.kr/product-management/course/period-name":
+    //     null,
+    //   "https://profile.caihong.co.kr/product-management/course/period-code":
+    //     null,
+    //   "https://profile.caihong.co.kr/product-management/course/display": null,
+    //   "https://profile.caihong.co.kr/product-management/course/sale": null,
+    //   "https://profile.caihong.co.kr/product-management/course/price": null,
+    //   "https://profile.caihong.co.kr/product-management/course/review-period":
+    //     null,
+    //   "https://profile.caihong.co.kr/product-management/course/registration-date":
+    //     null,
+    //   "https://profile.caihong.co.kr/content-management/course/lessons/pages": 20,
+    // },
     // NOTE kjw 오답점검때만 있는 데이터
     // incorrectPageDatas: [
     //   {
